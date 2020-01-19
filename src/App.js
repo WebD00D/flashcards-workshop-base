ab9585baa22460402d371916115710aab835fa4e
WebD00D <christian@stabmag.com>
2020-01-18 19:52:20 -0800

    cleans up for starter template

--- src/App.js
+++ src/App.js
@@ -4,58 +4,28 @@ import * as UI from "./lib/styles/styles";
 import FlashCards from "./components/flashcards";
 
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
-  const [question, setQuestion] = useState(false);
-
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
-    try {
-      setLoading(true);
-      const response = await fetch(
-        "https://opentdb.com/api.php?amount=10&category=9"
-      );
-      if (response.ok) {
-        const json = await response.json();
-        setQuestions(json.results);
-        initStack(json.results);
-      }
-    } catch (error) {
-      // swallowed 🤢
-    } finally {
-      setLoading(false);
-    }
+    // Fetches a question stack from Open Trivia API
   };
 
   const initStack = questionStack => {
-    const index = Math.floor(Math.random() * Math.floor(questionStack.length));
-    setQuestion(questionStack[index]);
+    // Should pick and set a random question from the stack for initial display
   };
 
   const setNextQuestion = () => {
-    const index = Math.floor(Math.random() * Math.floor(questions.length));
-    setQuestion(questions[index]);
-    removeCardFromStack(index);
+    // should pick a new random question, set it visible, and remove it from available list of questions
+    // to choose from
   };
 
-  const removeCardFromStack = index => {
-    const questionStack = [...questions];
-    questionStack.splice(index, 1);
-    setQuestions(questionStack);
-  };
+  const removeCardFromStack = index => {};
 
   return (
     <UI.Wrapper data-testid="app">
-      <FlashCards
-        question={question}
-        questions={questions || []}
-        handleNextQuestion={setNextQuestion}
-        handleFetchCards={getData}
-        loading={loading}
-      />
+      <FlashCards />
     </UI.Wrapper>
   );
 };
--- src/components/flashcards/index.js
+++ src/components/flashcards/index.js
@@ -1,55 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import "antd/dist/antd.css";
-import * as UI from "../../lib/styles/styles";
-import { Spin } from "antd";
 
-import { CardWrapper, Card, Side } from "./styles";
+import { CardWrapper } from "./styles";
 
-const FlashCards = ({
-  question,
-  questions,
-  handleNextQuestion,
-  handleFetchCards,
-  loading
-}) => {
-  const decodeText = text => {
-    const txt = document.createElement("textarea");
-    txt.innerHTML = text;
-    return txt.value;
-  };
-
-  return (
-    <>
-      <CardWrapper data-test-id="flashcard">
-        <Card className="card">
-          <div className="front">
-            <Side>Question</Side>
-            <div>
-              {!loading ? question && decodeText(question.question) : <Spin />}
-            </div>
-          </div>
-          <div className="back">
-            <Side>Answer</Side>
-            <div>{decodeText(question.correct_answer)}</div>
-          </div>
-        </Card>
-      </CardWrapper>
-      <UI.ButtonWrap>
-        <UI.Button
-          disabled={questions.length === 0}
-          onClick={handleNextQuestion}
-        >
-          Next Question
-        </UI.Button>
-        {!loading && questions.length === 0 && (
-          <UI.Reset onClick={handleFetchCards}>Refresh Deck</UI.Reset>
-        )}
-      </UI.ButtonWrap>
-
-      <UI.Label>REMAINING IN STACK: {questions.length}</UI.Label>
-    </>
-  );
+const FlashCards = () => {
+  return <CardWrapper>I'm going to be a flashcard app!</CardWrapper>;
 };
 
 export default FlashCards;
